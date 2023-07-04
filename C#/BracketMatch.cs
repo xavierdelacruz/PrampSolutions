9c0b4e750988760c20a85be47c72561c0fa1a39e
xavierdelacruz <xdelacruz89@gmail.com>
2023-07-04 09:03:35 -0700

    Added bracket match problem

---
+++ C#/BracketMatch.cs
@@ -0,0 +1,40 @@
+using System;
+
+class Solution
+{
+    public static int BracketMatch(string text)
+    {
+      if (string.IsNullOrWhiteSpace(text)) {
+        return 0;
+      }
+      
+      int leftCount = 0;
+      int rightCount = 0;
+      
+      foreach (var t in text) {
+        if (t.Equals('(')) {         
+          leftCount++;
+        }
+        
+        if (t.Equals(')') && leftCount > 0) {
+          leftCount--;
+          continue;
+        }
+        
+        if (t.Equals(')') && leftCount <= 0) {
+          rightCount++;
+          continue;
+        }   
+      }
+      
+      return rightCount + leftCount;
+      
+      
+    }
+
+    static void Main(string[] args)
+    {
+
+    }
+}
+
